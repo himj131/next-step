869afe61ac86f2d818c4752476a0ac630c2474f4
star <himj131@gmail.com>
2020-02-22 10:43:44 +0900

    refactoring

--- src/main/java/himj/nextstep/calculator/StringCalculator.java
+++ src/main/java/himj/nextstep/calculator/StringCalculator.java
@@ -16,22 +16,42 @@ import java.util.regex.Pattern;
 public class StringCalculator {
 
     public int add(String str) {
-        int total = 0;
-        if(str.trim().isEmpty()) return 0;
-        String[] values;
+        if(isBlank(str)) return 0;
+        return sum(toInts(splitString(str)));
+    }
+
+    private String[] splitString(String str) {
         Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(str);
         if(matcher.find()) {
             String separator = matcher.group(1);
-            values = matcher.group(2).split(separator);
-        } else {
-            values = str.split(",|:");
+            return matcher.group(2).split(separator);
         }
-        for (String value : values) {
-            int intValue = Integer.parseInt(value);
-            total += intValue;
-            if (intValue < 0) throw new RuntimeException();
-        }
+        return str.split(",|:");
+    }
 
+    private boolean isBlank(String str) {
+        return (str == null || str.trim().isEmpty());
+    }
+
+    private int sum(int[] values) {
+        int total = 0;
+        for (int value : values) {
+            total += value;
+        }
         return total;
+    }
+
+    private int[] toInts (String[] values) {
+        int[] ints = new int[values.length];
+        for(int i = 0; i < ints.length; i++) {
+            ints[i] = toPositive(values[i]);
+         }
+        return ints;
+    }
+
+    private int toPositive(String value) {
+        int number = Integer.parseInt(value);
+        if(number < 0) throw new RuntimeException();
+        return number;
     }
 }
---
+++ src/test/java/himj/nextstep/calculator/SplitTest.java
@@ -0,0 +1,13 @@
+package himj.nextstep.calculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class SplitTest {
+    @Test
+    void splitArrayTest() {
+        assertArrayEquals(new String[] {"1"}, "1".split(","));
+        assertArrayEquals(new String[] {"1","2"}, "1,2".split(","));
+    }
+}
