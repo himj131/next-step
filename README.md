5da8e9036775085475eae9d22bdcd6d9971b5de3
himj131@gmail.com <himj131@gmail.com>
2020-03-19 09:34:57 +0900

    index.html 응답하기
    BufferedReader.readLine(), Files.readAllBytes() 사용

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # next-step
 [Next Step](http://www.yes24.com/Product/Goods/31869154?scode=032&OzSrank=1) 책 구현해보기
+
+## 웹서버 실습
+####알게된 것
--- src/main/java/himj/nextstep/webserver/RequestHandler.java
+++ src/main/java/himj/nextstep/webserver/RequestHandler.java
@@ -1,10 +1,8 @@
 package himj.nextstep.webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,9 +21,24 @@ public class RequestHandler extends Thread {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+            String line = br.readLine();
+            if(line == null) return;
+            String[] splited = line.split(" ");
+            String path = splited[1];
+
+//            while (!"".equals(line)) {
+//                log.debug("header: {}", line);
+
+//                if(splited[1].equals("/index.html")) {
+//                    byte[] body = Files.readAllBytes(new File("./webapp" + "/index.html").toPath());
+//
+//                }
+//
+//                line = br.readLine(); //다음 줄 읽기, 없으면 무한루프에 빠진다.
+//            }
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+            byte[] body = Files.readAllBytes(new File("./webapp", path).toPath());
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
