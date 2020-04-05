6b55dbcdaf8b3dbb9344ced7980f3fd4eed5ba72
himj131@gmail.com <himj131@gmail.com>
2020-04-05 21:35:21 +0900

    session 설정 로직 추가

--- src/main/java/himj/nextstep/webserver/RequestHandler.java
+++ src/main/java/himj/nextstep/webserver/RequestHandler.java
@@ -1,18 +1,15 @@
 package himj.nextstep.webserver;
 
-import himj.nextstep.db.DataBase;
-import himj.nextstep.model.User;
 import himj.nextstep.util.HttpRequestUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
-import java.util.Collection;
 import java.util.Map;
+import java.util.UUID;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -32,6 +29,10 @@ public class RequestHandler extends Thread {
             HttpRequest request = new HttpRequest(in);
             HttpResponse response = new HttpResponse(out);
 
+            if(getSessionId(request.getHeader("Cookie")) == null) {
+                response.addHeader("Set-Cookie", "JSESSIONID=" + UUID.randomUUID());
+            }
+
             Controller controller = RequestMapping.getController(request.getPath());
 
             if(controller == null) {
@@ -43,6 +44,11 @@ public class RequestHandler extends Thread {
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private String getSessionId(String cookieValue) {
+        Map<String, String> cookies = HttpRequestUtils.parseCookies(cookieValue);
+        return cookies.get("JSESSIONID");
     }
 
     private String getDefaultPath(String path) {
---
+++ src/test/java/himj/nextstep/webserver/UUIDTest.java
@@ -0,0 +1,12 @@
+package himj.nextstep.webserver;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+public class UUIDTest {
+    @Test
+    void uuid() {
+        System.out.println(UUID.randomUUID());
+    }
+}
