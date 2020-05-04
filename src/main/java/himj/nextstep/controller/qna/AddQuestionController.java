ee35b055c4447f80e969bf17fd90c9385315af0d
himj131@gmail.com <himj131@gmail.com>
2020-05-04 17:49:27 +0900

    9.1.4 미로그인시 질문등록 로그인화면으로 리다이렉트처리

--- src/main/java/himj/nextstep/controller/qna/AddQuestionController.java
+++ src/main/java/himj/nextstep/controller/qna/AddQuestionController.java
@@ -5,12 +5,14 @@ import himj.nextstep.infra.AnswerDao;
 import himj.nextstep.infra.QuestionDao;
 import himj.nextstep.model.Answer;
 import himj.nextstep.model.Question;
+import himj.nextstep.model.User;
 import himj.nextstep.mvc.ModelAndView;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class AddQuestionController implements Controller {
     private static final Logger log = LoggerFactory.getLogger(AddQuestionController.class);
