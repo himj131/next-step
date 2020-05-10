72be888fab3a9f4c9dbef228b0a342da8c8ade51
himj131@gmail.com <himj131@gmail.com>
2020-05-10 14:01:30 +0900

    10.1.2.3 @MyTest애노테이션으로 설정된 메소드 실행하기

--- src/test/java/core/ref/Junit4TestRunner.java
+++ src/test/java/core/ref/Junit4TestRunner.java
@@ -2,10 +2,17 @@ package core.ref;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
+
 public class Junit4TestRunner {
     @Test
     public void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
-
+        Method[] methods = clazz.getMethods();
+        for(Method method : methods) {
+            if(method.isAnnotationPresent(MyTest.class)) {
+                method.invoke(clazz.getDeclaredConstructor().newInstance());
+            }
+        }
     }
 }
