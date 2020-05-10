0f6d743af9113d6e42e4e7c448c89a6167c32320
himj131@gmail.com <himj131@gmail.com>
2020-05-10 13:53:22 +0900

    10.2.2.2  "test"로 시작하는 메소드 실행하기

--- src/test/java/core/ref/Junit3TestRunner.java
+++ src/test/java/core/ref/Junit3TestRunner.java
@@ -2,10 +2,19 @@ package core.ref;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
+
 public class Junit3TestRunner {
     @Test
     public void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
+        Method[] methods = clazz.getMethods();
 
+        for(Method method : methods) {
+            if(method.getName().startsWith("test")) {
+                // "clazz.newInstance()" deplicated since 9
+                method.invoke(clazz.getDeclaredConstructor().newInstance());
+            }
+        }
     }
 }
