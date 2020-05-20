9882836aa6a3f1566f182cd2ee47fefe1e5d0ba6
himj131@gmail.com <himj131@gmail.com>
2020-05-21 08:43:37 +0900

    컴파일 에러 수정

--- src/main/java/next/controller/HomeController.java
+++ src/main/java/next/controller/HomeController.java
@@ -3,22 +3,17 @@ package next.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import next.dao.JdbcQuestionDao;
-import core.annotation.Controller;
-import core.annotation.RequestMapping;
+import core.mvc.AbstractController;
 import core.mvc.ModelAndView;
-import core.nmvc.AbstractNewController;
 import next.dao.QuestionDao;
 
-@Controller
-public class HomeController extends AbstractNewController {
+public class HomeController extends AbstractController {
     private QuestionDao questionDao;
 
     public HomeController(QuestionDao questionDao) {
         this.questionDao = questionDao;
     }
 
-    @RequestMapping("/")
     public ModelAndView execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
         return jspView("home.jsp").addObject("questions", questionDao.findAll());
     }
