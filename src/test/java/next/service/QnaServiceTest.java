37eb4d9aa9ce335f0f69b746f9a2af3f02aafc42
himj131@gmail.com <himj131@gmail.com>
2020-05-21 21:06:23 +0900

    11.3.3 DI보다 우선하는 객체지향 개발(2) 도메인로직 Question으로 이동

--- src/test/java/next/service/QnaServiceTest.java
+++ src/test/java/next/service/QnaServiceTest.java
@@ -1,19 +1,23 @@
 package next.service;
 
-import com.google.common.collect.Lists;
 import next.CannotDeleteException;
 import next.dao.MockAnswerDao;
 import next.dao.MockQuestionDao;
+import next.model.Answer;
 import next.model.Question;
+import next.model.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static next.model.QuestionTest.newQuestion;
+import java.util.Date;
+import java.util.List;
+
 import static next.model.UserTest.newUser;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -40,7 +44,13 @@ public class QnaServiceTest {
 
     @Test
     void deleteQuestion_다른사용자() {
-        Question question = newQuestion(1L, "mj");
+        User user = newUser("mj");
+        Question question = new Question(1L, user.getUserId(), "title",
+                "contents", new Date(), 0) {
+            public boolean canDelete(User user, List<Answer> answers) throws CannotDeleteException {
+                throw new CannotDeleteException("삭제할 수 없음");
+            };
+        };
         when(mockQuestionDao.findById(1L)).thenReturn(question);
         assertThrows(CannotDeleteException.class, () -> {
             qnaService.deleteQuestion(1L, newUser("userId"));
@@ -49,8 +59,15 @@ public class QnaServiceTest {
 
     @Test
     void deleteQuestion() throws CannotDeleteException {
-        Question question = newQuestion(1L, "mj");
+        User user = newUser("mj");
+        Question question = new Question(1L, user.getUserId(), "title",
+                "contents", new Date(), 0) {
+            public boolean canDelete(User user, List<Answer> answers) throws CannotDeleteException{
+                return true;
+            };
+        };
         when(mockQuestionDao.findById(1L)).thenReturn(question);
         qnaService.deleteQuestion(1L, newUser("mj"));
+        verify(mockQuestionDao).delete(question.getQuestionId());
     }
 }
