48e50ff25508cd90bf48b728d32508b05816c03a
himj131@gmail.com <himj131@gmail.com>
2020-05-21 09:33:04 +0900

    11.3.2 Mockito를 활용한 테스트

--- src/test/java/next/service/QnaServiceTest.java
+++ src/test/java/next/service/QnaServiceTest.java
@@ -1,30 +1,38 @@
 package next.service;
 
+import com.google.common.collect.Lists;
 import next.CannotDeleteException;
 import next.dao.MockAnswerDao;
 import next.dao.MockQuestionDao;
 import next.model.Question;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static next.model.QuestionTest.newQuestion;
 import static next.model.UserTest.newUser;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
 
-class QnaServiceTest {
-    private QnaService qnaService;
+@ExtendWith(MockitoExtension.class)
+public class QnaServiceTest {
+    @Mock
     private MockQuestionDao mockQuestionDao;
+    @Mock
     private MockAnswerDao mockAnswerDao;
+
+    private QnaService qnaService;
 
     @BeforeEach
     void setUp() {
-        mockQuestionDao = new MockQuestionDao();
-        mockAnswerDao = new MockAnswerDao();
         qnaService = new QnaService(mockQuestionDao, mockAnswerDao);
     }
 
     @Test
     void delteQuestion_없는질문() {
+        when(mockQuestionDao.findById(1L)).thenReturn(null);
         assertThrows(CannotDeleteException.class, () -> {
             qnaService.deleteQuestion(1L, newUser("userId"));
         });
@@ -32,8 +40,8 @@ class QnaServiceTest {
 
     @Test
     void deleteQuestion_다른사용자() {
-        Question quesiton = newQuestion(1L, "mj");
-        mockQuestionDao.insert(quesiton);
+        Question question = newQuestion(1L, "mj");
+        when(mockQuestionDao.findById(1L)).thenReturn(question);
         assertThrows(CannotDeleteException.class, () -> {
             qnaService.deleteQuestion(1L, newUser("userId"));
         });
@@ -41,8 +49,8 @@ class QnaServiceTest {
 
     @Test
     void deleteQuestion() throws CannotDeleteException {
-        Question quesiton = newQuestion(1L, "mj");
-        mockQuestionDao.insert(quesiton);
+        Question question = newQuestion(1L, "mj");
+        when(mockQuestionDao.findById(1L)).thenReturn(question);
         qnaService.deleteQuestion(1L, newUser("mj"));
     }
 }
